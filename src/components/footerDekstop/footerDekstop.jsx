e64c601bf09284be0c66d368da8338db7d54112a
snaidy giraldo <snaidy98g@gmail.com>
2023-07-14 17:28:45 -0600

    add changes footer

--- src/components/footerDekstop/footerDekstop.jsx
+++ src/components/footerDekstop/footerDekstop.jsx
@@ -9,19 +9,19 @@ const FooterDekstop = () => {
     <div className='footerDek__le'>
     <h1>loopstudios</h1>
     <section className='footerDek__le-span'>
-    <span className="footerDek__derecho-sub">About</span>
-    <span className="footerDek__derecho-sub">Careers</span>
-    <span className="footerDek__derecho-sub">Events</span>
-    <span className="footerDek__derecho-sub">Products</span>
-    <span className="footerDek__derecho-sub">Support</span>
+    <span >About</span>
+    <span >Careers</span>
+    <span >Events</span>
+    <span >Products</span>
+    <span >Support</span>
     </section>
         </div>
     <div className='footerDek__izquierdo'>
     <figure className="footerDek__izquierdo-img">
-      <FaSquareFacebook />
-      <BsTwitter />
-      <BsPinterest />
-      <BsInstagram />
+      <FaSquareFacebook className='icon'/>
+      <BsTwitter className='icon'/>
+      <BsPinterest className='icon'/>
+      <BsInstagram className='icon'/>
     </figure>
     <span className="footerDek__izquierdo-text">
       © 2021 Loopstudios. All rights reserved.
