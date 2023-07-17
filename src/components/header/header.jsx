9d8f047a157d7a3d4e0c611d6cb38f3dcdce13c2
jeffs22 <132307775+jeffs22@users.noreply.github.com>
2023-07-17 10:29:41 -0500

    integrar responsive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './header.scss'
 const Header = () => {
+
+
+  
   return (
-    <div>Header</div>
+    <nav className="container">
+    <div className="logo">
+      <h2>loopstudios</h2>
+      <p className='parr'>IMMERSIVE <br /> EXPERIENCES <br /> THAT DELIVER</p>
+    </div>
+    
+    <ul>
+      <li href="#">Menu</li>
+      <li href="#">Location</li>
+      <li href="#">About</li>
+      <li href="#">Contact</li>
+    </ul> 
+    
+
+    
+  </nav>
+  
+
+  
   )
 }
 
